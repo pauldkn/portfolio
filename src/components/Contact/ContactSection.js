a9e3ea2a1fcc2ea8fa951c0fb42d83d681b3fbb6
Paul Dkn <pauldkn.code@gmail.com>
2020-09-24 13:07:12 +0200

    Contact Section

--- src/components/Contact/ContactSection.js
+++ src/components/Contact/ContactSection.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ContactForm from "./ContactForm"
+import ContactForm from "./ContactForm";
 
 export default function ContactSection() {
   return (
     <section className="contact-section flex-col">
       <h1>Contact</h1>
-      <ContactForm />
+      <p>Paul Dickerson</p>
+      <a href="mailto:pauldkn.code@gmail.com">Email</a>
+      <ul>
+        <li><a href="/">LinkedIn</a> </li>
+        <li><a href="/">Github</a> </li>
+        <li><a href="/">Resume</a> </li>
+      </ul>
+      {/* <ContactForm /> */}
     </section>
   );
 }
