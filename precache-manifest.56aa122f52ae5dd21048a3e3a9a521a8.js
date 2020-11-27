5d564d72ede2bd436b703ffae3b9d99339cec52d
Paul Dkn <pauldkn.code@gmail.com>
2020-11-27 18:38:24 +0100

    Updates

---
+++ precache-manifest.56aa122f52ae5dd21048a3e3a9a521a8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db3bc96c54065185ba6f61ec3cd680b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a59ff5bfab634de927",
+    "url": "/static/css/main.d17437d1.chunk.css"
+  },
+  {
+    "revision": "00d572211ccfaad83a85",
+    "url": "/static/js/2.1d2fa984.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d2fa984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a59ff5bfab634de927",
+    "url": "/static/js/main.40b022d0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "58867b52af507472de0e4ffa8a99eb26",
+    "url": "/static/media/CV-Paul-Dickerson.58867b52.pdf"
+  },
+  {
+    "revision": "802eda160480ad526aea70cd84af0d25",
+    "url": "/static/media/bg-greetings.802eda16.jpg"
+  },
+  {
+    "revision": "bcc111c39cb86024d9db3d02695741c3",
+    "url": "/static/media/bike-project.bcc111c3.png"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/static/media/css.c26a73b4.png"
+  },
+  {
+    "revision": "021976e06ae09501f015ca79fbf077e9",
+    "url": "/static/media/express.021976e0.png"
+  },
+  {
+    "revision": "9cab3941a9641e93c85fb1b45f2da6a2",
+    "url": "/static/media/github.9cab3941.png"
+  },
+  {
+    "revision": "b23cf0127644d9ebfdd8c40fd5d29d0b",
+    "url": "/static/media/html.b23cf012.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "/static/media/js.db7933e6.png"
+  },
+  {
+    "revision": "fdfd8925f8720001ca1662af0b879526",
+    "url": "/static/media/mongodb.fdfd8925.png"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/static/media/nodejs.521444a2.png"
+  },
+  {
+    "revision": "34ecaea936f1e386a3256634be5965c9",
+    "url": "/static/media/react2.34ecaea9.png"
+  },
+  {
+    "revision": "1ddd596f384806aae9c8f06240ddbddc",
+    "url": "/static/media/skate-project.1ddd596f.png"
+  },
+  {
+    "revision": "79ea522eac39b360873e15f4b43a1470",
+    "url": "/static/media/snake-project.79ea522e.png"
+  },
+  {
+    "revision": "97b00882407afb7e6c4e81e46ca5879b",
+    "url": "/static/media/vscode.97b00882.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.03887ce4bca76906523cdda3031e6661.js"
+  "/precache-manifest.56aa122f52ae5dd21048a3e3a9a521a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
